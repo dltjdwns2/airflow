f7711d1409ca9ca91be8438f7b9e6299bf51f552
lsj <tbehfdl2@naver.com>
2025-01-21 19:43:12 +0900

    op_kwargs 예제 dag 업로드

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -14,8 +14,8 @@ with DAG(
     regist2_t1 = PythonOperator(
         task_id='regist2_t1',
         python_callable=regist2,
-        op_args=['hjkim','man','kr','seoul']
-        op_kwargs=['email':'hjkim_sun@naver.com','phone':'010']
+        op_args=['hjkim','man','kr','seoul'],
+        op_kwargs={'email':'hjkim_sun@naver.com','phone':'010'}
     )
 
     regist2_t1
