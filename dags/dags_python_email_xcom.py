171eadc679bd65980127b95432a13ad3fc1a15a2
lsj <tbehfdl2@naver.com>
2025-01-23 20:24:17 +0900

    수정

--- dags/dags_python_email_xcom.py
+++ dags/dags_python_email_xcom.py
@@ -4,10 +4,8 @@ import datetime
 from airflow.decorators import task
 from airflow.operators.email import EmailOperator
 
-
-
 with DAG(
-    dag_id="dags_python_email_operator",
+    dag_id="dags_python_email_xcom",
     schedule="0 8 1 * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
