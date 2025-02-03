c8111c6b3284298ccf0b2b2e6270cb8de13f2c55
lsj <tbehfdl2@naver.com>
2025-02-03 23:06:32 +0900

    변경사항 추가

--- plugins/config/get_kakao_token.py
+++ plugins/config/get_kakao_token.py
@@ -1,5 +1,5 @@
 import requests
-# client_id, authorize_code 노출 주의, 실제 값은 임시로만 넣고 Git에 올라가지 않도록 유의
+# client_id, authorize_code 노출 주의, 실제 값은 임시로만 넣고 Git에 올라가지 않도록 유의 올리면 안됨
 
 client_id = '{client_id}'
 redirect_uri = 'https://example.com/oauth'
